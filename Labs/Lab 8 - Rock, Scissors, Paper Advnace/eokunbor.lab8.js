abfbd0ae2a3a308a6524a375563c15f774953823
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2025-04-10 23:03:04 -0500

    Update

--- Labs/Lab 8 - Rock, Scissors, Paper Advnace/eokunbor.lab8.js
+++ Labs/Lab 8 - Rock, Scissors, Paper Advnace/eokunbor.lab8.js
@@ -32,9 +32,7 @@ function playGame(playerChoice) {
     if (playerChoice === computerChoice) {
         result = "It's a tie!";
     } else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
+        (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
         result = "You win!";
     } else {
